4a00fb3864589d3b93a1be2fd1505e92a1619c9c
Natalia Castro <nat.alvz@gmail.com>
2018-06-03 01:18:59 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+require 'pry'
+class CashRegister
+  
+  attr_accessor :total, :discount, :descuento
+  
+  def initialize(dis=nil)
+    @total = 0 
+    @discount = dis 
+  end
+  
+  def add_item(title, price, quantity=nil)
+    if quantity == nil
+      @total += price
+    else
+      @total += price*quantity
+    end
+  end
+  
+  def apply_discount
+    @descuento = (@total*100)/@discount
+    @descuento-@total
+    binding.pry
+  end 
+end
 
+  
